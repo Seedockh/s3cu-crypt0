d4185d90c96fa59cf38b1b52297f2b1d5bc08199
pierr <pierre.herisse@gmail.com>
2020-05-29 11:15:42 +0200

    add author and ex1

---
+++ ex01.hex2base64.js
@@ -0,0 +1,5 @@
+const hexString = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+
+const base64 = Buffer.from(hexString, 'hex').toString('base64')
+
+console.log(base64)
